a33d7d9eaffae717d0a9fbb9968c74c031ee9141
sanjeevi03 <sanjeevisan65@gmail.com>
2022-02-04 00:23:19 +0530

    First commit

---
+++ src/Crud.jsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function Crud() {
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState([]);
+  const [sortBox, setSortBox] = useState(false);
+  const [asc, setAsc] = useState(true);
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      const res = await axios.get(`https://restcountries.com/v3.1/all`);
+      setValue(res.data);
+      setLoading(false);
+    };
+    loadData();
+  }, []);
+  return (
+    <div>
+      <h2>Country Details Fetch Using Axios</h2>
+      <div className="sortBox">
+        <button onClick={() => setSortBox(!sortBox)} className="sort-button">Sort By</button>
+        {sortBox ? (
+          <div>
+            <button onClick={() => setAsc(!asc)}>{asc?"Descending By Country Name":"Ascending By Country Name"}</button>
+            <br />
+          </div>
+        ) : null}
+      </div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>S.No</th>
+                <th>Country</th>
+                <th>Capital</th>
+                <th>Region</th>
+                <th>Flag</th>
+              </tr>
+            </thead>
+            <tbody>
+              {loading ? (
+                <h1>Loading...</h1>
+              ) : asc ? (
+                value
+                  .sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
+                  .map((i, j) => (
+                    <tr key={j}>
+                      <td>{j + 1}</td>
+                      <td>{i.name.common}</td>
+                      <td>{i.capital ? i.capital : "Not Mentioned"}</td>
+                      <td>{i.region}</td>
+                      <td>
+                        <img src={i.flags.png} alt="flag" />
+                      </td>
+                    </tr>
+                  ))
+              ) : (
+                value
+                  .sort((a, b) => (a.name.common < b.name.common ? 1 : -1))
+                  .map((i, j) => (
+                    <tr key={j}>
+                      <td>{j + 1}</td>
+                      <td>{i.name.common}</td>
+                      <td>{i.capital ? i.capital : "Not Mentioned"}</td>
+                      <td>{i.region}</td>
+                      <td>
+                        <img src={i.flags.png} alt="flag" />
+                      </td>
+                    </tr>
+                  ))
+              )}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+}
+//
+export default Crud;
